99ab788560ace8538db4f2845e30f1322b9ca969
cubodado <dalkang000@gmail.com>
2022-02-07 21:57:30 +0900

    feat: create ClickToEdit component

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Toggle from './component/Toggle';
+import ClickToEdit from './component/ClickToEdit';
 
 function App() {
   return (
     <div className="App">
       <Toggle />
+      <ClickToEdit />
     </div>
   );
 }
---
+++ custom-component/src/component/ClickToEdit.js
@@ -0,0 +1,9 @@
+function ClickToEdit() {
+  return (
+    <div>
+      <h1>ClickToEdit</h1>
+    </div>
+  );
+}
+
+export default ClickToEdit;
