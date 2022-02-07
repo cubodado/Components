02e2b986e546ca80fd9eb294f6e048f479cfc80a
cubodado <dalkang000@gmail.com>
2022-02-07 21:37:59 +0900

    feat: complete toggle button

--- custom-component/src/component/Toggle.js
+++ custom-component/src/component/Toggle.js
@@ -1,11 +1,27 @@
 import "./Toggle.css"
+import React, { useState } from "react";
 
 function Toggle() {
+  const [checkStateStr, setCheckStateStr] = useState("Off");
+  let checkState = false;
+
+  const onChangeCheckState = () => {
+    checkState = !checkState;
+
+    if (checkState === true) {
+      setCheckStateStr("On");
+    } else {
+      setCheckStateStr("Off");
+    }
+  };
+
   return (
     <div className="Toggle">
       <h1>Toggle</h1>
-      <input type="checkbox" id="check"></input>
-      <label htmlFor="check">Button</label>
+      <input type="checkbox" id="check-toggle" onChange={ onChangeCheckState }></input>
+      <label htmlFor="check-toggle">
+      </label>
+      <p>Toggle Switch { checkStateStr }</p>
     </div>
   );
 }
