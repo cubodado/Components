d5e045c2479f006d6e045f5d9ff164e83d8e39a5
cubodado <dalkang000@gmail.com>
2022-02-07 19:55:42 +0900

    feat: create checkbox for making toggle button

--- custom-component/src/component/Toggle.js
+++ custom-component/src/component/Toggle.js
@@ -1,7 +1,11 @@
+import "./Toggle.css"
+
 function Toggle() {
   return (
     <div className="Toggle">
       <h1>Toggle</h1>
+      <input type="checkbox" id="check"></input>
+      <label htmlFor="check">Button</label>
     </div>
   );
 }
