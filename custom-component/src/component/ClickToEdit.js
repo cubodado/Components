4a87a93893c4d66571d19dfde603708ab12f7790
cubodado <dalkang000@gmail.com>
2022-02-08 15:46:38 +0900

    feat: complete ClickToEdit feat

--- custom-component/src/component/ClickToEdit.js
+++ custom-component/src/component/ClickToEdit.js
@@ -1,7 +1,40 @@
+import React, { useState } from 'react';
+import "./ClickToEdit.css";
+
 function ClickToEdit() {
+  const body = document.querySelector("body");
+  const [inputName, setInputName] = useState();
+  const [inputAge, setInputAge] = useState();
+
+  const mousedownBodyEvent = (event) => {
+    const target = event.target;
+    const nameInput = document.querySelector("#name").value;
+    const ageInput = document.querySelector("#age").value;
+    
+    if (target === event.currentTarget.querySelector("#name") || 
+        target === event.currentTarget.querySelector("#age")) {
+      return;
+    } 
+    setInputName(nameInput);
+    setInputAge(ageInput);
+  };
+
+  body.addEventListener('mousedown', mousedownBodyEvent);
+
   return (
-    <div>
+    <div className="clickToEdit">
       <h1>ClickToEdit</h1>
+      <section>
+        <label htmlFor="name" id="name-label">이름</label>
+        <input type="text" id="name"></input>
+      </section>
+      <section>
+        <label htmlFor="age" id="age-label">나이</label>
+        <input type="text" id="age"></input>
+      </section>
+      <section>
+        <p>이름 { inputName } 나이 {inputAge}</p>
+      </section>
     </div>
   );
 }
