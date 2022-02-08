18f4095dfd462a018a9caad92aaaf0aa4eafde2f
cubodado <dalkang000@gmail.com>
2022-02-08 15:51:14 +0900

    feat: create Modal component

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Toggle from './component/Toggle';
 import ClickToEdit from './component/ClickToEdit';
+import Modal from './component/Modal';
 
 function App() {
   return (
     <div className="App">
       <Toggle />
       <ClickToEdit />
+      <Modal />
     </div>
   );
 }
