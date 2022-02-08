1fd2f140ff181b6b36bbf662f4e8a22f42c38b1d
cubodado <dalkang000@gmail.com>
2022-02-08 20:39:09 +0900

    feat: complete Modal feat

--- custom-component/src/component/Modal.js
+++ custom-component/src/component/Modal.js
@@ -1,11 +1,33 @@
 import './Modal.css';
 
 function Modal() {
+  const openModal = () => {
+    const modal = document.querySelector(".modal-popup");
+    modal.classList.remove("hidden");
+  };
+
+  const closeModal = () => {
+    const modal = document.querySelector(".modal-popup");
+    modal.classList.add("hidden");
+  };
+
   return (
-    <div className="Modal">
+    <div className="modal">
       <h1>Modal</h1>
+      <section className="modal-box">
+        <button className="modal-btn" onClick={ openModal }>Open Modal</button>
+        <section className="modal-popup hidden">
+          {/* 모달 밖 클릭할 때도 창 종료할 경우에는 아래 코드로 변경/실행 */}
+          {/* <div className="modal-background" onClick={ closeModal }></div> */}
+          <div className="modal-background"></div>
+          <div className="modal-popup-content">
+            <button className="close-btn" onClick={ closeModal }>X</button>
+            <p>HELLO CODESTATES!</p>
+          </div>
+        </section>
+      </section>
     </div>
   );
-};
+}
 
 export default Modal;
