2ca9c15bb2cd05e34057c6f4f53e6b5141403976
cubodado <dalkang000@gmail.com>
2022-02-09 22:43:17 +0900

    feat: complete Tab feat

--- custom-component/src/component/Tab.js
+++ custom-component/src/component/Tab.js
@@ -1,9 +1,54 @@
 import './Tab.css';
 
 function Tab() {
+  const onChangeColor = event => {
+    const eventTarget = event.currentTarget;
+    const eventTargetId = eventTarget.dataset.target;
+    const tabs = document.querySelectorAll(".tab-div");
+    const contents = document.querySelectorAll(".content-div");
+
+    tabs.forEach(tab => {
+      if (tab.dataset.target === eventTargetId) {
+        eventTarget.classList.add("active");
+      } else {
+        tab.classList.remove("active");
+      }
+    })
+
+    contents.forEach(content => {
+      if (content.id === eventTargetId) {
+        content.classList.add("selected");
+      } else {
+        content.classList.remove("selected");
+      }
+    })
+  };
+
   return (
     <div className="tab">
       <h1>Tab</h1>
+      <section className="tab-bar">
+        <div className="tab-div active" data-target="tab1" onClick={onChangeColor}>
+          <p>Tab1</p>
+        </div>
+        <div className="tab-div" data-target="tab2" onClick={onChangeColor}>
+          <p>Tab2</p>
+        </div>
+        <div className="tab-div" data-target="tab3" onClick={onChangeColor}>
+          <p>Tab3</p>
+        </div>
+      </section>
+      <section className="tab-content">
+        <div id="tab1" className="content-div selected">
+          <p>Tab menu ONE</p>
+        </div>
+        <div id="tab2" className="content-div">
+          <p>Tab menu TWO</p>
+        </div>
+        <div id="tab3" className="content-div">
+          <p>Tab menu THREE</p>
+        </div>
+      </section>
     </div>
   );
 }
